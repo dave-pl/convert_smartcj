bdc7dd6d310d64695fd54f4c788c63a55132d87e
dave-pl <35957391+dave-pl@users.noreply.github.com>
2018-03-18 23:13:28 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -43,3 +43,10 @@ done
 echo "converting tables to innodb"
 cat $TMPFILE |while read l; do read -d, c1 c2 < <(echo $l) ; echo "$c1 $c2 "; $MYSQL -u$USER -p$PASS -e "use $c1; alter table $c2 ENGINE = INNODB;" ; done
 rm -rf $TMPFILE
+#
+#версия 2 
+#Mysql Table Engine: looks like mysql DB tables (rot_gallery_info, rot_gallery_stats1) are not of InnoDB format. it's good idea to tune mysql and use innodb table format for rot_* tables. Please, read wiki for mysql tuning hints.
+#Script Installation is done. Please, open http://dave.test/scj2/admin/ in your browser. Your login is 'admin' and password is 'cxyfsgaevi'.
+#версия 1.5
+#Script Installation is done. Please, open http://dave2.test/scj/admin/ in your browser. Your login is 'admin' and password is 'adminpass'. 
+#Mysql Table Engine: looks like mysql DB tables (rot_galleries, rot_gallery_stats) are not of InnoDB format. it's good idea to tune mysql and use innodb table format for rot_* tables. Please, read wiki for mysql tuning hints.
